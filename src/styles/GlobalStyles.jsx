b0a1fc7892181f0c07d4c6511865baf994940acc
Daniel <danielgonzalezt13@gmail.com>
2021-08-28 00:37:53 +0200

    Added custom slidebar

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -4,19 +4,29 @@ const GlobalStyles = createGlobalStyle`
   body{
       margin: 0;
       padding: 0;
-      background:#141414;
+      background-color:#141414;
       color: #c7c7c7;
-  }  
+
+        &::-webkit-scrollbar{
+            width: 8px;
+            background:#141414;
+        }
+
+        &::-webkit-scrollbar-thumb{
+            width: 4px;
+            background-color: #282c34;
+            border-radius: 10px;
+        }
+    }  
 
   *{
       box-sizing: border-box;
       font-family: 'Space Grotesk', sans-serif;
-
-  }
+    }
 
   h1,p{
       margin: 0;
-  }
+    }
 `
 
 export default GlobalStyles
