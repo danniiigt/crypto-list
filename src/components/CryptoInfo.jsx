432dc5f6f01fd46d26f79aff24593ed2e0101d9b
Daniel <danielgonzalezt13@gmail.com>
2021-09-12 12:20:16 +0200

    Added Forecast & CriptoInfo

---
+++ src/components/CryptoInfo.jsx
@@ -0,0 +1,75 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+const CryptoInfoWrapper = styled.div`
+    padding: 0 18px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid #47484b;
+
+    .crypto-name{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .crypto-info{
+            display: flex;
+            align-items: center;
+        }
+
+        .crypto-price{
+            h1{
+                font-weight: 600;
+            }
+        }
+
+        img{
+            max-width: 30px;
+            max-height: 30px;
+            margin-right: 10px;
+        }
+
+        h1{
+            font-size: 1.5em;
+            font-weight: 500;
+        }
+    }
+`
+
+
+
+const CryptoInfo = ({location}) =>{
+
+    const [crypto, setCrypto] = useState({})
+
+    const getCryptoData = async () =>{
+        const res = await fetch(`https://api.coingecko.com/api/v3/coins/${location.state.cryptoID}`)
+        const data = await res.json()
+        setCrypto(data)
+    }
+
+    useEffect(() => {
+        getCryptoData()
+    }, [location])
+
+    return(
+        <>  
+            <CryptoInfoWrapper>
+                {crypto.image && (
+                    <div className="crypto-name">
+                        <div className="crypto-info">
+                            <img src={crypto.image.small} alt="" />
+                            <h1>{crypto.name}</h1>
+                        </div>
+                        <div className="crypto-price">
+                            <h1>{crypto.market_data.current_price.usd}$</h1>
+                        </div>
+                    </div>
+                )}
+                
+            </CryptoInfoWrapper>
+            
+        </>
+    )
+}
+
+export default CryptoInfo
---
+++ src/components/Forecast.jsx
@@ -0,0 +1,126 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+const ForecastWrapper = styled.div`
+    margin-bottom: 20px;
+    padding: 0 18px;
+    min-height: 25px;
+    height: fit-content;
+    margin-top: 20px;
+
+    .forecast{
+        display: ${({isPredicted}) => !isPredicted ? "flex" : "none"};
+
+    }
+
+    .forecast-percentage{
+        display: ${({isPredicted}) => isPredicted ? "block" : "none"};
+    }
+
+    h1{
+        font-size: .9em;
+        font-weight: lighter;
+        display: inline-block;
+        margin-right: 15px;
+    }
+
+    button{
+        display: inline-block;
+        margin-right: 8px;
+        border: none;
+        color: white;
+        cursor: pointer;
+        border-radius: 3px;
+    }
+
+    button.buy{
+        background-color: #00800060;
+    }
+
+    button.sell{
+        margin-right: 0;
+        background-color: #ff000067;
+    }
+
+    .forecast-bar{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .buy-bar{
+        width: ${({predictionData}) => predictionData.buy}%;
+        height: 4px;
+        background-color: green;
+
+        label{
+            text-align: right;
+            font-size: 0.7em;
+        }
+    }
+
+    .sell-bar{
+        width: ${({predictionData}) => predictionData.sell}%;
+        height: 4px;
+        background-color: red;
+
+        label{
+            font-size: 0.7em;
+        }
+    }
+`
+
+
+const Forecast = ({location}) =>{
+
+    const [isPredicted, setIsPredicted] = useState(false)
+    const [predictionData, setPredictionData] = useState({})
+
+
+    const getPredictionData = async () =>{
+        const res = await fetch(`https://api.coingecko.com/api/v3/coins/${location.state.cryptoID}`)
+        const data = await res.json()
+        const prediction = {
+            buy: data.sentiment_votes_up_percentage,
+            sell: data.sentiment_votes_down_percentage
+        }
+
+        setPredictionData(prediction)
+    }
+
+    const showForecast = () =>{
+        setIsPredicted(true)
+    }
+
+    useEffect(() => {
+        setIsPredicted(false)
+        getPredictionData()
+    }, [location]);
+    
+
+    return(
+        <>
+            <ForecastWrapper isPredicted={isPredicted} predictionData={predictionData}>
+                <div className="forecast">
+                    <h1>What is your forecast for {location.state.cryptoName}?</h1>
+                    <button className="buy" onClick={showForecast}>BUY</button>
+                    <button className="sell" onClick={showForecast}>SELL</button>
+                </div>
+                <div className="forecast-percentage">
+                    <div className="forecast-bar">
+                        <div className="buy-bar">
+                            <label htmlFor="">Buy - {predictionData.buy}%</label>
+                        </div>
+                        <div className="sell-bar">
+                            <label htmlFor="">Sell - {predictionData.sell}%</label>
+                        </div>
+                    </div>
+                </div>
+            </ForecastWrapper>
+        </>
+    )
+}
+
+export default Forecast
